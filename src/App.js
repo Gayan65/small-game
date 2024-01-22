a9f8f82facdf5fc970fbfcdf4368e7b07a0451a8
gayan65728@yahoo.com <egawi001@edu.xamk.fi>
2024-01-22 11:55:13 +0200

    modfiy the squre function to pass a new fution

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import React, { useState } from "react";
 
-//Returns the value x once the butoon get clicked
-function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        setValue("X");
-    }
-
+//Returns the value once the button get clicked
+function Square({ value, onSquareClick }) {
     return (
-        <button className="square" onClick={handleClick}>
+        <button className="square" onClick={onSquareClick}>
             {value}
         </button>
     );
 }
 
 export default function Board() {
+    //Makinga new state array
+    const [squares, setSquares] = useState(Array(9).fill(null));
     return (
         <>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[0]} />
+                <Square value={squares[1]} />
+                <Square value={squares[2]} />
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[3]} />
+                <Square value={squares[4]} />
+                <Square value={squares[5]} />
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[6]} />
+                <Square value={squares[7]} />
+                <Square value={squares[8]} />
             </div>
         </>
     );
